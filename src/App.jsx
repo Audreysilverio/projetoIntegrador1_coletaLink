1f80008ced0ad9eb15f7093ec0b9847d926e405c
Audreysilverio <audrey_silverio@yahoo.com.br>
2025-04-27 19:05:25 -0300

    Atualização do Front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { AiOutlineSearch } from 'react-icons/ai';
+import { FaInfoCircle, FaProjectDiagram, FaMapMarkedAlt, FaHandHoldingHeart, FaSearch, FaUsers } from "react-icons/fa";
 import "./App.css";
 import QuemSomos from "./pages/Quem Somos/QuemSomos.jsx";
 import Doar from "./pages/Doar/Doar.jsx";
@@ -12,13 +13,19 @@ import Informacoes from "./pages/Informacoes/Informacoes.jsx"
 function App() {
   return (
     <>
+      <div className="container">
+        <header className="header">
+        <img src={logo} alt="Logo ColetaLink" className="logo" />
+        </header>
+      </div>
+      
       {/* Navbar */}
       <div className="auth-buttons">
         <Link to="/login" className="btn">Login</Link>
         <Link to="/cadastro" className="btn">Cadastro</Link>
       </div>
 
-      <img src={logo} alt="Logo ColetaLink" className="logo" />
+      
 
       <div className="search-container">
   <input type="text" placeholder="Pesquisar locais de coleta..." />
@@ -28,11 +35,12 @@ function App() {
       {/* Navegação principal */}
       <div className="buttons-grid">
         <Link to="/quem-somos" className="btn">Quem Somos</Link>
-        <Link to="/doar" className="btn">Doar</Link>
-        <Link to="/buscar" className="btn">Buscar</Link>
-        <Link to="/mapa" className="btn">Mapa</Link>
         <Link to="/projeto" className="btn">Projeto</Link>
         <Link to="/informacoes" className="btn">Informações</Link>
+        <Link to="/mapa" className="btn">Mapa</Link>
+        <Link to="/doar" className="btn">Doar</Link>
+        <Link to="/buscar" className="btn">Buscar</Link>
+        
       </div>
 
 
@@ -40,11 +48,12 @@ function App() {
       <Routes>
         {/* Você pode trocar o path "/" por outro componente real futuramente */}
         <Route path="/quem-somos" element={<QuemSomos />} />
+        <Route path="/projeto" element={<Projeto />} />
+        <Route path="/informacoes" element={<Informacoes />} />
+        <Route path="/mapa" element={<Mapa />} />
         <Route path="/doar" element={<Doar />} />
         <Route path="/buscar" element={<Buscar />} />
-          <Route path="/mapa" element={<Mapa />} />
-          <Route path="/projeto" element={<Projeto />} />
-          <Route path="/informacoes" element={<Informacoes />} />
+         
       </Routes>
 
       <footer>
